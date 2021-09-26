727344f26a7ea5cd518524db57cfbb784cb249a2
Nadia <nlnadialigia@gmail.com>
2021-09-26 19:07:59 -0300

    resolution Challenge 115-b

--- professor/cadastrar.py
+++ professor/cadastrar.py
@@ -1,21 +1,2 @@
-from formatacao import titulo, line
 
-pessoas = dict()
-
-
-def cadastrar():
-    arquivo = open('pessoas.txt', 'a')
-    titulo('NOVO CADASTRO')
-    try:
-        pessoas['nome'] = str(input('Nome: '))
-        pessoas['idade'] = int(input('Idade: '))
-        arquivo.writelines(f'{pessoas["nome"]:20} {pessoas["idade"]}\n')
-        print(f'Novo registro de {pessoas["nome"]} adicionado.')
-    except TypeError:
-        print('Dados inválidos. Tente novamente')
-    except FileNotFoundError:
-        arquivo = open('pessoas.txt', 'a')
-        arquivo.writelines(pessoas)
-    arquivo.close()
-    return pessoas
 
--- professor/main.py
+++ professor/main.py
@@ -1,10 +1,16 @@
 from lib.interface import *
+from lib.arquivo import *
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
         # listar()
-        cabecalho('Opção 1')
+        lerArquivo(arq)
     elif resposta == 2:
         # cadastrar()
         cabecalho('Opção 2')
